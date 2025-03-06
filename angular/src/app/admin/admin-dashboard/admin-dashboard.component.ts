38156582bc8b3e1bfb600162f0b821692712afc5
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-03-06 12:36:22 -0500

    upgrade angular to v19

--- angular/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ angular/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -4,9 +4,10 @@ import { AdminService } from '../services/admin.service';
 import { DiscoRequestDto } from '../models/admin.types';
 
 @Component({
-  selector: 'app-admin-dashboard',
-  templateUrl: './admin-dashboard.component.html',
-  styleUrls: ['./admin-dashboard.component.css']
+    selector: 'app-admin-dashboard',
+    templateUrl: './admin-dashboard.component.html',
+    styleUrls: ['./admin-dashboard.component.css'],
+    standalone: false
 })
 export class AdminDashboardComponent {
 
