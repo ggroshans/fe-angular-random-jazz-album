248847e3dd1181b8fbbbfa7e5f8df9e2e7c0683c
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-03-10 15:15:27 -0400

    setup homepage routing

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./client/client.module').then((m) => m.ClientModule),
+  },
+  {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
   },
--- angular/src/app/client/client.module.ts
+++ angular/src/app/client/client.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { ItemDetailComponent } from './shared/item-detail/item-detail.component';
+import { AlbumDetailComponent } from './album/album-detail/album-detail.component';
+import { HomeComponent } from './home/home.component';
 import { ClientRoutingModule } from './client-routing.module';
 
-
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, AlbumDetailComponent, ItemDetailComponent],
   imports: [
     CommonModule,
-    ClientRoutingModule
-  ]
+    ClientRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule,
+  ],
 })
 export class ClientModule { }
