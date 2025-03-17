6f145d8910d88ecd93761d6c1235c3b5ae6db5ae
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-03-17 11:45:27 -0400

    add client models

---
+++ angular/src/app/client/models/Album.ts
@@ -0,0 +1,21 @@
+import { Artist } from "./Artist";
+import { Genre } from "./Genre";
+import { Mood } from "./Mood";
+import { Subgenre } from "./Subgenre";
+
+export interface Album {
+  id: number;
+  title: string;
+  description: string;
+  theme: string;
+  imageUrl: string;
+  label: string;
+  popularTracks: string[];
+  releaseYear: string;
+  totalTracks: number;
+  popularityScore: number;
+  genre: Genre;
+  subgenres: Subgenre[];
+  artists: Artist[];
+  moods: Mood[];
+}
---
+++ angular/src/app/client/models/Artist.ts
@@ -0,0 +1,10 @@
+import { Genre } from "./Genre";
+
+export interface Artist {
+  id: number;
+  name: string;
+  biography: string;
+  genres: Genre[];
+  imageUrl: string;
+  popularityScore: number;
+}
