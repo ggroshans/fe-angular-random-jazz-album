b6450c33e7286cdbe5cd5bc1a029cf7d24829271
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-03-21 10:56:50 -0400

    import store modules

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,13 +5,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),],
   providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule { }
