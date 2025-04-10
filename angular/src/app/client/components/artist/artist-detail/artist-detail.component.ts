a4f939ac560f3163a567783b6fd8d6192f0f8e86
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-04-10 14:30:14 -0400

    generate artist-detail component

---
+++ angular/src/app/client/components/artist/artist-detail/artist-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-detail',
+  imports: [],
+  templateUrl: './artist-detail.component.html',
+  styleUrl: './artist-detail.component.css'
+})
+export class ArtistDetailComponent {
+
+}
