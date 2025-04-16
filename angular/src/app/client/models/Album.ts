5745a087219297fb271a10fb6149b15a7c50c0e7
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-04-16 17:26:01 -0400

    add streaming svgs and links

--- angular/src/app/client/models/Album.ts
+++ angular/src/app/client/models/Album.ts
@@ -4,19 +4,30 @@ import { Mood } from "./Mood";
 import { Subgenre } from "./Subgenre";
 
 export interface Album {
-  id: number;
-  title: string;
-  description: string;
-  theme: string;
-  imageUrl: string;
-  label: string;
-  popularTracks: string[];
-  releaseYear: string;
-  totalTracks: number;
-  popularityScore: number;
-  percentileScore: number;
-  genre: Genre;
-  subgenres: Subgenre[];
-  artists: Artist[];
-  moods: Mood[];
+  id?: number;
+  title?: string;
+  description?: string;
+  theme?: string;
+  imageUrl?: string;
+  label?: string;
+  popularTracks?: string[];
+  releaseYear?: string;
+  totalTracks?: number;
+  popularityScore?: number;
+  percentileScore?: number;
+  genre?: Genre;
+  spotifyId: string;
+  youtubeId: string;
+  appleMusicId: string;
+  amazonMusicId: string;
+  pandoraId: string;
+  subgenres?: Subgenre[];
+  artists?: Artist[];
+  moods?: Mood[];
+
+
+  // duration?: string;
+  // suggestedListeningTime?: string; // explicit
+  // jazzEra?: string[]; // explicit
+  // moodScore?: number;
 }
