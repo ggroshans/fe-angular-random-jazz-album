64f9311156ed977eaa00f4e74e14a4fb02ba13d0
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-04-23 15:00:12 -0400

    add totalAlbums property to Artist

--- angular/src/app/client/models/Artist.ts
+++ angular/src/app/client/models/Artist.ts
@@ -1,11 +1,30 @@
+import { Album } from "./Album";
 import { Genre } from "./Genre";
 
 export interface Artist {
   id: number;
-  name: string;
-  biography: string;
-  genres: Genre[];
-  imageUrl: string;
-  popularityScore: number;
+  name?: string;
+  biography?: string;
+  genres?: Genre[];
+  imageUrl?: string;
+  instrument?: string;
+  popularityScore?: number;
   percentileScore: number;
+
+  totalAlbums?: number; // Computed
+  // averageAlbumScore?: number;  // Computed
+  // subgenreBreakdown?: { [subgenre: string]: number }; // explicit subgenres // Computed
+  // jazzEra?: string[]; // explicit
+  // birthDate?: string; // GPT
+  // deathDate?: string; // GPT
+  // yearsActive?: { startDate: number, endDate: number }; // GPT
+  // moodBreakdown?: { [mood: string]: number }; // Computed
+  // averageMoodScore?: number;  // Computed
+  // funFact?: string; // GPT
+  albums?: Album[];
+  noteableAlbums?: Album[];
+
+  // relatedArtists: string[]; // GPT
+  // influences: string[]; // GPT
+  // influenced: string[]; // GPT
 }
