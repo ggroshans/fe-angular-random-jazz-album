83f35807d776e7ed6a53691fdf8b065cecf451e8
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-05-09 13:17:30 -0400

    add background css transition when theme colors change

--- angular/src/app/client/components/home/home.component.ts
+++ angular/src/app/client/components/home/home.component.ts
@@ -1,4 +1,7 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectSecondaryColor } from '../../state/color/color.selectors';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +10,15 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomeComponent {
+  secondaryColor: string = "";
 
+  constructor(private store: Store) { }
+
+  ngOnInit() {
+    this.store.select(selectSecondaryColor).subscribe(
+      (secondaryColor) => {
+        this.secondaryColor = secondaryColor
+        console.log("color changed");
+      })
+  }
 }
